b96b700bb2ff1be04fb7f5a882409c4f477d1141
DarkSky <darksky2048@gmail.com>
2017-09-13 12:24:00 +0800

    sort app list

--- VirtualApp/app/src/main/java/io/virtualapp/home/HomeActivity.java
+++ VirtualApp/app/src/main/java/io/virtualapp/home/HomeActivity.java
@@ -258,6 +258,7 @@ public class HomeActivity extends VActivity implements HomeContract.HomeView {
 
     @Override
     public void loadFinish(List<AppData> list) {
+        list.sort((o1, o2) -> o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()));
         list.add(new AddAppButton(this));
         mLaunchpadAdapter.setList(list);
         hideLoading();
